fd40a3199db13072bd04788f1ab4d88039306ff4
Jack Kai Lim <limjackailjk@gmail.com>
2025-02-18 23:51:31 -0800

    add done check

--- dags/anime_top_25_dag.py
+++ dags/anime_top_25_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from airflow.operators.base_operator import BaseOperator
 
 # Import the functions for ETL
 from anime_top_25.extract import get_top_data
@@ -11,41 +12,46 @@ from anime_top_25.load import load_data
 # Default arguments for the DAG.
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2025, 2, 6),
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(
+with DAG(
     'anime_top25_etl',
     default_args=default_args,
     schedule_interval='@daily',  # or adjust as needed
     catchup=False
-)
+) as dag:
 
-# Task: Extract data from the Jikan API.
-extract_top_data = PythonOperator(
-    task_id='extract_top_data',
-    python_callable=get_top_data,
-    provide_context=True,
-    dag=dag,
-)
+    # Task: Extract data from the Jikan API.
+    extract_top_data = PythonOperator(
+        task_id='extract_top_data',
+        python_callable=get_top_data,
+        provide_context=True,
+        dag=dag,
+    )
 
-# Task: Transform the extracted data.
-transform_top_data = PythonOperator(
-    task_id='transform_top_data',
-    python_callable=parse_data,
-    provide_context=True,
-    dag=dag,
-)
+    # Task: Transform the extracted data.
+    transform_top_data = PythonOperator(
+        task_id='transform_top_data',
+        python_callable=parse_data,
+        provide_context=True,
+        dag=dag,
+    )
 
-# Task: Load the transformed data into DynamoDB.
-load_top_data = PythonOperator(
-    task_id='load_top_data',
-    python_callable=load_data,
-    provide_context=True,
-    dag=dag,
-)
+    # Task: Load the transformed data into DynamoDB.
+    load_top_data = PythonOperator(
+        task_id='load_top_data',
+        python_callable=load_data,
+        provide_context=True,
+        dag=dag,
+    )
 
-# Set task dependencies: extract then transform.
-extract_top_data >> transform_top_data >> load_top_data
+    mark_done = BaseOperator(
+        task_id='mark_done',
+        bash_command='touch /home/ubuntu/done_check/done.marker',
+        dag=dag
+    )
+
+    # Set task dependencies: extract then transform.
+    extract_top_data >> transform_top_data >> load_top_data >> mark_done
